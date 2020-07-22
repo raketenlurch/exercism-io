385068ea2a10fa9f0ed1f52d9d885a640f8b103a
raketenlurch <raketenlurch@riseup.net>
2020-07-22 14:07:36 +0200

    Add first python exercises

---
+++ exercism/python/raindrops/raindrops.py
@@ -0,0 +1,16 @@
+"""def convert(number):
+    string_list = []
+    if number % 3 == 0:
+        string_list.append('Pling')
+    elif number % 5 == 0:
+        string_list.append('Plang')
+    elif number % 7 == 0:
+        string_list.append('Plong')
+    else:
+        string_list.append(number)
+    string = ''.join(string_list)
+    return string"""
+
+def convert(number):
+    string_list = []
+
